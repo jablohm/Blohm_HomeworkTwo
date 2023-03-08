4404d6d6e8e051ff625956a7fc3bb854f5e4fd33
jablohm <77591463+jablohm@users.noreply.github.com>
2023-03-08 18:08:27 -0500

    nav update 1

--- app/app.js
+++ app/app.js
@@ -1,20 +1,32 @@
-function changeRoute() {
-    let hashTag = window.location.hash;
-    let pageID = hashTag.replace("#", "");
-    // console.log(hashTag + " " + pageID); 
-    $.get(`pages/${pageID}/${pageID}.html`, function(data){
-        console.log("data" + data);
-        $("#app").html(data);
-    });
-}
+// function changeRoute() {
+//     let hashTag = window.location.hash;
+//     let pageID = hashTag.replace("#", "");
+//     // console.log(hashTag + " " + pageID); 
+//     $.get(`pages/${pageID}/${pageID}.html`, function(data){
+//         console.log("data" + data);
+//         $("#app").html(data);
+//     });
+// }
 
-function initURLlistener() {
-    $(window).on("hashchange", changeRoute);
-    changeRoute();
-}
+// function initURLlistener() {
+//     $(window).on("hashchange", changeRoute);
+//     changeRoute();
+// }
 
+
+// $(document).ready(function() {
+//     initURLlistener();
+
+// })
+
+function initListeners () {
+    $(".bars").click(function(e) {
+        // console.log("clicked");
+        $(".bars").toggleClass("active");
+        // turns added class on and off
+    })
+}
 
 $(document).ready(function() {
-    initURLlistener();
-
-})
+    initListeners();
+     });
