72781728533d36d834275914979b24c094e84569
jablohm <77591463+jablohm@users.noreply.github.com>
2023-03-08 19:39:59 -0500

    css keeps deleting lmao

--- app/app.js
+++ app/app.js
@@ -1,23 +1,17 @@
-// function changeRoute() {
-//     let hashTag = window.location.hash;
-//     let pageID = hashTag.replace("#", "");
-//     // console.log(hashTag + " " + pageID); 
-//     $.get(`pages/${pageID}/${pageID}.html`, function(data){
-//         console.log("data" + data);
-//         $("#app").html(data);
-//     });
-// }
+function changeRoute() {
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#", "");
+    // console.log(hashTag + " " + pageID); 
+    $.get(`pages/${pageID}/${pageID}.html`, function(data){
+        console.log("data" + data);
+        $("#app").html(data);
+    });
+}
 
-// function initURLlistener() {
-//     $(window).on("hashchange", changeRoute);
-//     changeRoute();
-// }
-
-
-// $(document).ready(function() {
-//     initURLlistener();
-
-// })
+function initURLlisteners() {
+    $(window).on("hashchange", changeRoute);
+    changeRoute();
+}
 
 function initListeners () {
     $(".bars").click(function(e) {
@@ -38,4 +32,5 @@ function initListeners () {
 
 $(document).ready(function() {
     initListeners();
+    initURLlisteners();
      });
